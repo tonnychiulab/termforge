e0f66806b2cfbfbe1835cfb77c65951b2248df8c
tonnychiulab <tonnychiulab@users.noreply.github.com>
2026-09-17 13:02:10 +0800

    feat: WebAssembly support, retro CRT web player, and OpenSpec SDD (add-web-wasm-support)

---
+++ pkg/termforge_web_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_wasmasteroidsgame_free: (a: number, b: number) => void;
+export const wasmasteroidsgame_key_down: (a: number, b: number, c: number) => void;
+export const wasmasteroidsgame_key_up: (a: number, b: number, c: number) => void;
+export const wasmasteroidsgame_last_sound: (a: number, b: number) => void;
+export const wasmasteroidsgame_new: (a: number, b: number) => number;
+export const wasmasteroidsgame_reset: (a: number) => void;
+export const wasmasteroidsgame_tick: (a: number, b: number, c: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_export3: (a: number, b: number, c: number) => void;
